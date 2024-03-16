d1f4933048ad7f31e8a98a08c50867a189a3ea25
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-03-16 15:14:27 -0300

    owner and verbose_name in Category model

--- contact/models.py
+++ contact/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+    class Meta():
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
     def __str__(self) -> str:
         return self.name
 class Contact(models.Model):
@@ -16,6 +20,7 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True,upload_to='picture/%Y/%m')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
