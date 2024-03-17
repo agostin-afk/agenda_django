46eb4073183f21dfa0f0ee0cb27b32ebfe88d29b
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-03-17 10:36:22 -0300

    improvement in fetch contacts

--- contact/admin.py
+++ contact/admin.py
@@ -3,11 +3,11 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContractAdmin(admin.ModelAdmin):
-    list_display = 'id','first_name','last_name', 'phone','category',
+    list_display = 'id','first_name','last_name', 'phone','category', 'show',
     list_filter = 'created_date',
     ordering = ()
     search_fields = 'id', 'first_name', 'last_name',
-    list_editable = 'phone',
+    list_editable = 'phone', 'show'
     list_per_page = 20
     list_max_show_all = 500
     list_display_links = 'first_name',
