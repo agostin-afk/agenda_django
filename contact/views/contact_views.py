10d5515e9dc308a14031d08d0376fc6a856dc278
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-03-17 16:58:56 -0300

    search

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
+from django.db.models import Q
 from django.http import Http404
 
 
@@ -30,3 +31,27 @@ def contact(request, contact_id):
         'contact/contact.html',
         context
         )
+def search(request):
+    search_result = request.GET.get('q', '').strip()
+    
+    if search_result == '':
+        return redirect('contact:index')
+    
+    contacts = Contact.objects.filter(show=True)\
+    .filter(
+            Q(first_name__icontains=search_result)|
+            Q(phone__icontains=search_result) |
+            Q(email__icontains=search_result) 
+            )
+    
+    context = {
+        'contacts': contacts,
+        'site_title': search_result +' - ',
+        'search_value' : search_result
+    }
+    return render(
+        request,
+        'contact/index.html',
+        context
+        )
+
