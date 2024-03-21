86ba3053fe0a7f490d2e3632109939d4a9392622
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-03-21 11:06:45 -0300

    attrs nos campos

--- contact/forms.py
+++ contact/forms.py
@@ -1,8 +1,24 @@
+from typing import Any, Mapping
+from django.core.files.base import File
+from django.db.models.base import Model
+from django.forms.utils import ErrorList
 from contact.models import Contact
 from django.core.exceptions import ValidationError
 from django import forms
 
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget= forms.TextInput(
+            attrs= {
+                'placeholder': 'Primeiro nome',
+            }
+        ),
+        help_text= 'insira o primeiro nome do contato'
+    )
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        #self.fields['first_name'].widget.atttr.update({
+        #})
     
     class Meta:
         model = Contact
