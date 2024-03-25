6d5d886cff8638a4746efdfd8f3d56ac0f0be394
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-03-25 16:44:50 -0300

    login do usuario completo

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from contact.forms import RegisterForm
-from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages, auth
 
 
 def register(request):
@@ -18,3 +19,25 @@ def register(request):
             'form': form,
         },
     )
+def login_view(request):
+    form = AuthenticationForm(request)
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data= request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            messages.success(request,'Logado com sucesso')
+            auth.login(request, user)
+            return redirect('contact:index')
+        messages.error(request, 'Login inválido')
+        
+    return render(
+        request,
+        'contact/login.html',
+        {
+            'form': form
+        }
+    )
+
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contact:login')
